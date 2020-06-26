0f8ff7a82a4b99ec4394aeebfeb8d215d4c57eed
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-25 20:43:26 -0400

    Coded the seeds and seeded successfully

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :categories
+
+  validates :name, presence: true, inclusion: { in: ['Livres', 'Électroniques', 'Maison', 'Vêtements', 'Sports', 'Jouets', 'Musique et films', 'Beauté', 'Autres']}
 end
