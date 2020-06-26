caf08339e25a404790b6e48bf95de25f2e3634e4
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-26 14:46:50 -0400

    Seeded and worked a bit on view (personalised to the user)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,3 @@
 class Category < ApplicationRecord
   has_many :categories
-
-  validates :name, presence: true, inclusion: { in: ['Livres', 'Électroniques', 'Maison', 'Vêtements', 'Sports', 'Jouets', 'Musique et films', 'Beauté', 'Autres']}
-  validates :position, presence: true, inclusion: { in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
 end
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,4 +1,6 @@
 class Wish < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  # validates :position, presence: true, inclusion: { in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
 end
