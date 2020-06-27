4ce9819a6ed6a6ae8aa67457647c05a3e0916449
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-27 19:48:35 -0400

    Tried fixing backend flow

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -7,8 +7,10 @@ class WishesController < ApplicationController
   end
 
   def create
-    # Category expected got "56", which is an instance of string
-    @wish = Wish.new(wishes_params)
+    @wish = Wish.new
+    @wish.name = params[:wish][:name]
+    @wish.category = Category.find((params[:wish][:category]).to_i)
+    @wish.position = (params[:wish][:position]).to_i
     @wish.user = current_user
 
     if @wish.save
@@ -22,13 +24,18 @@ class WishesController < ApplicationController
   end
 
   def edit
+    @categories = Category.all.map { |category| category.name }
   end 
   
   def update
-    @wish = Wish.update(wishes_params)
-
-    if @wish.save
-      redirect_to root_path #Change this to profile when User controller generated
+    @wish.category = Category.find_by(name: (params[:wish][:category]).capitalize)
+    
+    
+    @wish.position = (params[:wish][:position]).to_i
+    @wish.position = nil if (params[:wish][:position]).to_i == 0
+    
+    if @wish.update(wishes_params)
+      redirect_to user_path(current_user)
       flash.notice = "🤖 Ton voeu a été mis à jour! Espèrons que Père Noël l'exaucera!"
     else
       flash.alert = "🤖 Oh-oh... Il y a un petit problème interne... Peux-tu réessayer?"
@@ -38,7 +45,7 @@ class WishesController < ApplicationController
   
   def destroy
     if @wish.destroy
-      redirect_to root_path #Change this to profile when User controller generated
+      redirect_to user_path(current_user)
       flash.notice = "🤖 Ton voeu a bel et bien été détruit"
     else
       flash.alert = "🤖 Oh-oh... Il y a un petit problème interne... Peux-tu réessayer?"
@@ -51,6 +58,6 @@ class WishesController < ApplicationController
   end
 
   def wishes_params
-    params.require(:wish).permit(:name, :category)
+    params.require(:wish).permit(:name, :category_id, :position)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :categories
+  has_many :wishes
 end
