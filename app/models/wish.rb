ae089adf8903a8212cbe2aeb373529af2adb2fd0
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-26 20:10:16 -0400

    Added flow to faq page and started writing faq

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -3,6 +3,6 @@ class Wish < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { in: 3..40 }
-  # validates :position, inclusion: { in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}, uniqueness: { scope: :user}
+  # validates :position, inclusion: { in: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}, uniqueness: { scope: :user}
   # validates :position, presence: true
 end
