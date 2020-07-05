7da360ed736cf1a36113a4a388ae20eb646724b6
Stephanie Diep <stephanie.diep002@gmail.com>
2020-07-05 16:25:50 -0400

    Added backend for q and a

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,8 @@
+class QuestionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @questions = Question.all
+    @categories = Category.all.map { |category| category.name }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/faq', to: 'pages#faq', as: 'faq'
+  get '/faq', to: 'questions#index', as: 'faq'
   resources :wishes, only: [:new, :create, :edit, :update, :destroy]
   resources :users, only: [:show]
 end
